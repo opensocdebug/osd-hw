41acbe93f155c859543255eca4306a187fd88dbd
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-01-26 08:54:56 +0100

    Add HIM stub

---
+++ modules/him/verilog/osd_him.sv
@@ -0,0 +1,12 @@
+
+module osd_him
+  (input clk, rst,
+   glip_channel.slave glip_in,
+   glip_channel.master glip_out,
+
+   dii_channel.master dii_out,
+   dii_channel.slave dii_in);
+
+
+endmodule // osd_him
+

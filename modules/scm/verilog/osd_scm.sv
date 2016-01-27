81197c9aaeda3b7733de88d743742c65071caef9
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-01-27 16:43:47 +0100

    SCM: Use statusctrl interface

--- modules/scm/verilog/osd_scm.sv
+++ modules/scm/verilog/osd_scm.sv
@@ -1,12 +1,20 @@
 
 module osd_scm
-  #(parameter VENDORID='x,
-    parameter SYSTEMID='x,
+  #(parameter SYSTEMID='x,
     parameter NUM_MOD='x)
    (input clk, rst,
+
+    input [9:0] id,
+
     dii_channel debug_in,
     dii_channel debug_out);
    
+   osd_statctrlif
+     #(.MODID(16'hbeef), .MODVERSION(16'h1))
+   u_statctrlif(.*,
+                 .id (id),
+                 .debug_in (debug_in),
+                 .debug_out (debug_out));
    
 
 endmodule

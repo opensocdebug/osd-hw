5702046b722c861c4d22e86e425b5f6657b25461
Wei Song <wsong83@gmail.com>
2016-02-02 16:00:14 +0000

    add assemble() for dii_flit and use enum for state machine

--- interfaces/verilog/dii_channel.sv
+++ interfaces/verilog/dii_channel.sv
@@ -7,5 +7,16 @@ package dii_package;
       logic [15:0] data;
    } dii_flit;
 
+   function dii_flit
+     dii_flit_assemble(
+                       logic m_valid,
+                       logic m_last,
+                       logic m_data
+                       );
+      dii_flit_assemble.valid = m_valid;
+      dii_flit_assemble.last = m_last;
+      dii_flit_assemble.data = m_data;
+   endfunction // dii_flit_assemble
+
 endpackage // dii_package
 

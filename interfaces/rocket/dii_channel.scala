f8c987d98eecdaa4fdd4f5d22b4e519f896e115f
Wei Song <wsong83@gmail.com>
2016-03-21 17:17:48 +0000

    add blackbox and blackbox port connection

--- interfaces/rocket/dii_channel.scala
+++ interfaces/rocket/dii_channel.scala
@@ -31,3 +31,27 @@ class DiiIO extends Bundle {
   val dii_out = Decoupled(new DiiFlit)
   val dii_in = Decoupled(new DiiFlit).flip
 }
+
+class DiiIOBBox extends DiiBundle {
+  val dii_in = UInt(INPUT, width=(new DiiFlit).getWidth + 1)
+  val dii_in_ready = Bool(OUTPUT)
+  val dii_out = UInt(OUTPUT, width=(new DiiFlit).getWidth + 1)
+  val dii_out_ready = Bool(INPUT)
+}
+
+class DiiBBoxPort extends DiiModule {
+  val io = new bundle {
+    val bbox = (new DiiIOBBox).flip
+    val chisel = (new DiiIO).flip
+  }
+
+  val w = (new DiiFlit).getWidth + 1
+  io.bbox.dii_in := Cat(io.chisel.dii_in.valid, io.chisel.dii_in.bits.last, io.chisel.dii_in.bits.data)
+  io.chisel.dii_in.ready := io.bbox.dii_in_ready
+  io.bbox.dii_out_ready := io.chisel.dii_out.ready
+  io.chisel.dii_out.valid := io.bbox.dii_out(w-1)
+  io.chisel.dii_out.bits.last := io.bbox.dii_out(w-2)
+  io.chisel.dii_out.bits.data := io.bbox.dii_out
+}
+
+
--- modules/rocket/debug_module.scala
+++ modules/rocket/debug_module.scala
@@ -2,14 +2,19 @@ package open_soc_debug
 
 import Chisel._
 
+case object DebugCtmID extends Field[Int]
+case object DebugStmID extends Field[Int]
+
 trait HasDebugModuleParameters extends UsesParameters {
-  val regWidth = 64             // system word length
-  val regAddrWidth = 5          // 32 user registers
-  val csrAddrWidth = 12         // address width of CSRs
-  val stmUserRegAddr = 10       // the address of the user register for software trace
-  val stmThreadPtrAddr = 4      // the address of the thread pointer for software trace
-  val stmThreadPtrChgID = 0     // the software trace id when thread pointer changed
-  val stmCsrAddr = 0            // the CSR used for software trace
+  val regWidth = 64               // system word length
+  val regAddrWidth = 5            // 32 user registers
+  val csrAddrWidth = 12           // address width of CSRs
+  val stmUserRegAddr = 10         // the address of the user register for software trace
+  val stmThreadPtrAddr = 4        // the address of the thread pointer for software trace
+  val stmThreadPtrChgID = 0       // the software trace id when thread pointer changed
+  val stmCsrAddr = 0              // the CSR used for software trace
+  val ctmID = params(DebugCtmID)  // the debug module ID of the core trace module
+  val stmID = params(DebugStmID)  // the debug module ID of the software trace module
 }
 
 abstract class DebugModuleModule extends Module with HasDebugModuleParameters
@@ -17,5 +22,5 @@ abstract class DebugModuleBundle extends Bundle with HasDebugModuleParameters
 
 
 class DebugModuleIO extends DebugModuleBundle {
-  val net = new DiiIO
+  val net = new DiiIOBBox
 }

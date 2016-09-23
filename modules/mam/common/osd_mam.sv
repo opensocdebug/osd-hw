49b77d32bd674cddb68735ed2cc43e7645eb99ce
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-09-23 13:47:46 +0200

    Fix write_data assignment

--- modules/mam/common/osd_mam.sv
+++ modules/mam/common/osd_mam.sv
@@ -310,9 +310,7 @@ module osd_mam
         end
         STATE_WRITE: begin
            nxt_write_data_reg[(DATA_WIDTH/16-wcounter)*16-1 -: 16] = dp_in.data;
-           write_data[(DATA_WIDTH/16-wcounter)*16-1 -: 16] = dp_in.data;
-           //nxt_write_data_reg[(wcounter+1)*16-1 -: 16] = dp_in.data;
-           //write_data[(wcounter+1)*16-1 -: 16] = dp_in.data;
+           write_data_m[(DATA_WIDTH/16-wcounter)*16-1 -: 16] = dp_in.data;
            dp_in_ready = 1;
            if (dp_in.valid) begin
               nxt_wcounter = wcounter + 1;

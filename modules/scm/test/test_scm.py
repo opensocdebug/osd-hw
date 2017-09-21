636b140563bba34e004244d8de74f8e144edaba9
Philipp Wagner <philipp.wagner@tum.de>
2017-09-21 20:05:42 +0200

    Starting point for cocotb tests

---
+++ modules/scm/test/test_scm.py
@@ -0,0 +1,12 @@
+import cocotb
+from cocotb.triggers import Timer
+from cocotb.clock import Clock
+from cocotb.result import TestFailure
+from cocotb.triggers import RisingEdge
+
+# XXX: Include our testing library here
+
+@cocotb.test()
+def test_scm(dut):
+    # XXX: insert test code here
+
